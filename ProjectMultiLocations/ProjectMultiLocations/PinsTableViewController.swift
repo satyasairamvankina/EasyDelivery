53eba6f903ce525c177ce721851f011b6abf0404
Vankina,Satya Sai Ram <s531508@ol10032m.nwmissouri.edu>
2018-10-07 16:46:15 -0500

    worked on tableview controller just showing sample data by hard coding for now

--- ProjectMultiLocations/ProjectMultiLocations/PinsTableViewController.swift
+++ ProjectMultiLocations/ProjectMultiLocations/PinsTableViewController.swift
@@ -10,37 +10,35 @@ import UIKit
 
 class PinsTableViewController: UITableViewController {
 
+    let a = ["p1","p2","p3","p4","p5"]
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return a.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "pinCells", for: indexPath)
+        let model = DestinationLocationClass.destinationLocation.address()
+        cell.textLabel?.text = a[indexPath.row]
+        cell.detailTextLabel?.text = "\(model)"
         return cell
     }
-    */
+    
+    override func viewWillAppear(_ animated: Bool) {
+        tableView.reloadData()
+    }
 
     /*
     // Override to support conditional editing of the table view.
