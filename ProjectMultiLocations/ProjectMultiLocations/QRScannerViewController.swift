c95ca0fd7f63cd48d0c60051308ee563aacf56a2
Student <student@admins-macbook-air-2.local>
2018-10-07 14:22:58 -0500

    QRScannerViewController.swift file added. It is basically the scan page view where it ask the user permission to use the phone camera to scan the QR code and gives the address.

--- ProjectMultiLocations/ProjectMultiLocations/QRScannerViewController.swift
+++ ProjectMultiLocations/ProjectMultiLocations/QRScannerViewController.swift
@@ -7,14 +7,62 @@
 //
 
 import UIKit
+import AVFoundation
 
-class QRScannerViewController: UIViewController {
-
+class QRScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    
+    
+    @IBOutlet weak var square: UIImageView!
+    
+    //Create a variable for the preview layer
+    var video = AVCaptureVideoPreviewLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        // Creating session
+        let session = AVCaptureSession()
+        
+        //Define capture device
+        let captureDevice = AVCaptureDevice.default(for: AVMediaType.video)
+        
+        //adding the capture device to our session
+        do{
+            let input = try AVCaptureDeviceInput(device: captureDevice!)
+            session.addInput(input)
+        }
+        catch{
+            print("error")
+        }
+        
+        let output = AVCaptureMetadataOutput()
+        session.addOutput(output)
+        
+        output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+        output.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+        
+        video = AVCaptureVideoPreviewLayer(session: session)
+        video.frame = view.layer.bounds
+        view.layer.addSublayer(video)
+        self.view.bringSubviewToFront(square)
+        
+        session.startRunning()
     }
-
-
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        if metadataObjects != nil && metadataObjects.count != 0{
+            if let object = metadataObjects[0] as? AVMetadataMachineReadableCodeObject{
+                if object.type == AVMetadataObject.ObjectType.qr{
+                    let alert = UIAlertController(title: "QRCode", message: object.stringValue, preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Retake", style: .default, handler: nil))
+                    alert.addAction(UIAlertAction(title: "Copy", style: .default, handler: {(nil) in
+                        UIPasteboard.general.string = object.stringValue
+                    }))
+                }
+            }
+        }
+    }
 }
 
