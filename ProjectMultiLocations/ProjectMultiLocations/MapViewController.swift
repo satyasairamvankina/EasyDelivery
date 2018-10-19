f2d956bfba9a132d47e7be94e064bb53b8e5a991
Vankina,Satya Sai Ram <s531508@ch140031m.local>
2018-10-19 13:10:22 -0500

    updated code for routes

--- ProjectMultiLocations/ProjectMultiLocations/MapViewController.swift
+++ ProjectMultiLocations/ProjectMultiLocations/MapViewController.swift
@@ -11,9 +11,15 @@ import MapKit
 import CoreLocation
 
 class MapViewController: UIViewController, CLLocationManagerDelegate, UISearchBarDelegate {
-        @IBOutlet weak var mapView: MKMapView!
+//      var currentLocation:CLLocationCoordinate2D
+//    init(currentLocation:CLLocationCoordinate2D) {
+//        self.currentLocation = currentLocation
+//    }
+    
+
+    @IBOutlet weak var mapView: MKMapView!
     @IBAction func searchBarBTN(_ sender: Any) {
-        
+       
         //inserting search bar
         let searchController =  UISearchController(searchResultsController: nil)
         searchController.searchBar.delegate = self
@@ -23,57 +29,76 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, UISearchBa
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         
-        //Ignore user once search button is clicked
-        UIApplication.shared.beginIgnoringInteractionEvents()
-        
-        //activity controller to ignore user while loading and showing activity on process
-        let activityIndicator = UIActivityIndicatorView(style: .gray)
-        activityIndicator.center = self.view.center
-        activityIndicator.hidesWhenStopped = true
-        activityIndicator.startAnimating()
-        
-        //show activity indicator on screen
-        self.view.addSubview(activityIndicator)
+        activityIndicatorFNC()
         
         //resign keyborad
         searchBar.resignFirstResponder()
-        //hide searchbar
-        dismiss(animated: true, completion: nil)
         
-//remove activity indicator and stop user interaction
-        activityIndicator.stopAnimating()
-        UIApplication.shared.endIgnoringInteractionEvents()
- //Search address in maps
+        //Search address in maps
         let searchAddress = MKLocalSearch.Request()
         searchAddress.naturalLanguageQuery = searchBar.text
         let searchOutput = MKLocalSearch(request: searchAddress)
+        
         searchOutput.start { (response, Error) in
             if let address = response {
-//  remove all annotations/pins
-               var a =  self.mapView.annotations
+                //  remove all annotations/pins
+                var a =  self.mapView.annotations
                 a.removeAll()
                 
+                //if latitude and longitude are legitamate then place a pin
                 if let latitude = response?.boundingRegion.center.latitude,let longitude = response?.boundingRegion.center.longitude{
-                 let givenLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+                let givenLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+                    
+                // Adding annotation
                    let  annotate = MKPointAnnotation()
                     annotate.title = "Take from user"
                     annotate.subtitle = searchBar.text
-                    
                     annotate.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
                     self.mapView.addAnnotation(annotate)
                     
-//                    var annotateArray:[MKAnnotation] = []
-//                    annotateArray.append(annotate)
+                //adding elemeents into annotation array
                     DestinationLocationClass.annotateArray.append(annotate)
-                    print(  DestinationLocationClass.annotateArray)
+                    print(DestinationLocationClass.annotateArray)
                     
-//                    func mapView(mapView: MKMapView, didSelectAnnotationView view: MKAnnotationView) {
-//                        //        self.selectedAnnotation = view.annotation as? Islands
-//                        annotate.title = "chnged"
-//                    }
+                  //calling mapview did select func
+                    self.mapView(mapView: self.mapView, didSelectAnnotationView: MKAnnotationView.init(annotation: annotate, reuseIdentifier: "empty"))
                     
-//                    mapView(mapView: self.mapView, didSelectAnnotationView: MKAnnotationView.init(annotation: annotate, reuseIdentifier: "empty"))
+// working on directions
+//                    let sourcePlaceMark = MKPlacemark(coordinate: self.currentLocation)
+                    let sourcePlaceMark = MKPlacemark(coordinate: CLLocationCoordinate2DMake(latitude, longitude))
+                    let destinationPlaceMark = MKPlacemark(coordinate: givenLocation)
+
+                    let sourceItem = MKMapItem(placemark: sourcePlaceMark)
+                    let destItem = MKMapItem(placemark: destinationPlaceMark)
+
+                    let directionRequest = MKDirections.Request()
+                    directionRequest.source = sourceItem
+                    directionRequest.destination = destItem
+                    directionRequest.transportType = MKDirectionsTransportType.automobile
                     
+                    let directions = MKDirections(request: directionRequest)
+                    
+                    directions.calculate(completionHandler: {
+                        response, error in
+                        guard let response = response
+                            else{ if let error = error{
+                            print("Something went wrong")
+                        }
+                        return
+                        }
+                        let route = response.routes[0]
+                        self.mapView.addOverlay(route.polyline, level: .aboveRoads)
+                        
+                        let rectangle = route.polyline.boundingMapRect
+                        self.mapView.setRegion(MKCoordinateRegion(rectangle), animated: true)
+                    })
+                }
+                func mapView(_ mapView: MKMapView, rendererFor overlay:MKOverlay) -> MKOverlayRenderer{
+                    let renderer = MKPolylineRenderer(overlay: overlay)
+                    renderer.strokeColor = UIColor.blue
+                    renderer.lineWidth = 5.0
+                    
+                    return renderer
                 }
             }
             else{
@@ -83,8 +108,6 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, UISearchBa
             }
         }
         
-//        if let searchOutput.start()
-//        }
     }
 
     
@@ -102,14 +125,16 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, UISearchBa
 //        let region:MKCoordinateRegion = MKCoordinateRegion(center: location.coordinate,latitudinalMeters: locationRadius, longitudinalMeters: locationRadius)
 //         let currentLocation = DestinationLocationClass(title: "Current Location", coordinate: myLocation)
 
-//                  mapView.addAnnotation(currentLocation)
 
         let region:MKCoordinateRegion = MKCoordinateRegion(center: myLocation,span: span)
         mapView.setRegion(region, animated: true)
 
-        print(location.coordinate.latitude)
-        print(location.coordinate.longitude)
         self.mapView.showsUserLocation = true
+        
+//        func currentLocation() -> CLLocationCoordinate2D{
+//            return myLocation
+//        }
+//        currentLocation = myLocation
 
     }
         
@@ -118,6 +143,8 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, UISearchBa
         func centerLocation(location:CLLocation){
             let locationRegion = MKCoordinateRegion(center: location.coordinate,latitudinalMeters: locationRadius, longitudinalMeters: locationRadius)
             mapView.setRegion(locationRegion, animated: true)
+            
+            
         }
     
     override func viewDidLoad() {
@@ -130,8 +157,46 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, UISearchBa
         
          }
 
-
+    func activityIndicatorFNC(){
+        //Ignore user once search button is clicked
+        UIApplication.shared.beginIgnoringInteractionEvents()
+        
+        //activity controller to ignore user while loading and showing activity on process
+        let activityIndicator = UIActivityIndicatorView(style: .gray)
+        activityIndicator.center = self.view.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.startAnimating()
+        
+        //show activity indicator on screen
+        self.view.addSubview(activityIndicator)
+        
+        //hide searchbar
+        dismiss(animated: true, completion: nil)
+        
+        //remove activity indicator and stop user interaction
+        activityIndicator.stopAnimating()
+        UIApplication.shared.endIgnoringInteractionEvents()
+        }
+    
+    //clicking on mapp view
+    func mapView(mapView: MKMapView, didSelectAnnotationView view: MKAnnotationView) {
+        //        self.selectedAnnotation = view.annotation as?
+//        annotate.title = "chnged"
+        print("selected Annotation")
+    }
 
 }
 
+//
+//let regionDistance:CLLocationDistance = 1000;
+//let coordinates = CLLocationCoordinate2DMake(latitude, longitude)
+//let regionSpan = MKCoordinateRegion(center: coordinates, latitudinalMeters: regionDistance, longitudinalMeters: regionDistance)
+//
+//let options = [MKLaunchOptionsMapCenterKey: NSValue(mkCoordinate: regionSpan.center), MKLaunchOptionsMapSpanKey: NSValue(mkCoordinateSpan: regionSpan.span)]
+//
+//let placemark = MKPlacemark(coordinate: coordinates)
+//let mapItem = MKMapItem(placemark: placemark)
+//mapItem.name = "My House"
+//mapItem.openInMaps(launchOptions: options)
+//
 
--- ProjectMultiLocations/ProjectMultiLocations/PinsTableViewController.swift
+++ ProjectMultiLocations/ProjectMultiLocations/PinsTableViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import MapKit
 
 class PinsTableViewController: UITableViewController {
-
-    let a = (DestinationLocationClass.annotateArray)
     
-//        ["p1","p2","p3","p4","p5"]
+//    var i:[MKAnnotation] = []
+//    var b:[MKAnnotation] = (DestinationLocationClass.annotateArray)
+//    var a:[String] = []
+//        var c:[String] = []
+////    init(i:[MKAnnotation]) {
+////        self.i = i
+//        for i in b {
+//            a.append((i.title!)!)
+//            c.append((i.subtitle!)!)
+//        }
+////    }
+
+    
+    
+   let a = ["p1","p2","p3","p4","p5"]
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -33,7 +46,7 @@ class PinsTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "pinCells", for: indexPath)
         let model = DestinationLocationClass.destinationLocation.address()
-        cell.textLabel?.text = a[indexPath.row].title!
+        cell.textLabel?.text = a[indexPath.row]
         cell.detailTextLabel?.text = "\(String(describing: model))"
         return cell
     }
@@ -42,49 +55,5 @@ class PinsTableViewController: UITableViewController {
         tableView.reloadData()
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
